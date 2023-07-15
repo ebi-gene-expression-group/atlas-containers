1ad29e6c647141587d74caef8250b02049cac59d
Pablo Moreno <pablo.moreno@astrazeneca.com>
2023-07-16 00:09:38 +0100

    jupyter single cell based on galaxy-jupyter

--- miniconda_template/create_dockerfile.sh
+++ miniconda_template/create_dockerfile.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-cat miniconda_template/Dockerfile > $1/Dockerfile
+dockerfile=miniconda_template/Dockerfile
+if [ -f "$1/pre_install_dockerfile" ]; then
+    dockerfile=$1/pre_install_dockerfile
+fi
+cat $dockerfile > $1/Dockerfile
 
 if [ -f "$1/post_install_dockerfile" ]; then
   cat $1/post_install_dockerfile >> $1/Dockerfile
